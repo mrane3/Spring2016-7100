f6d853dce33f10fdeacd07de1a836f5836d60693
Milap Rane <mrane3@gatech.edu>
2016-02-10 12:30:42 -0500

    changes in annotate

--- annotate.m
+++ annotate.m
@@ -1,5 +1,5 @@
 
-
+echo on;
 bpm_max = 360 ;
 minTime = 60 / (bpm_max*2) ;
 
@@ -9,12 +9,14 @@ hopSize = 128 ;
 [file, path] = uigetfile('../DATA_CUT_FINAL/*.*', 'Select Audio File') ;
 [data, fs] = audioread([path, file]) ;
 
-% [file, path] = uigetfile('../DATA_CUT_FINAL/*.*', 'Select Notation File') ;
-% fp = fopen([path, file]) ;
-% notation = textscan(fp, '%s') ;
-% notation = notation{1} ;
-of=cd('hpss');
+[file, path] = uigetfile('../DATA_CUT_FINAL/*.*', 'Select Notation File') ;
+fp = fopen([path, file]) ;
+notation = textscan(fp, '%f') ;
+%notation = notation{1} ;
+
+oldfold=cd('hpss');
 [h,p]=s_hpss_IGprior(data,512,5);
-cd(of);
-onset_times = onset_detect(p, fs, windowSize, hopSize, minTime) ;
-onset_times=onset_times/fs;
+cd(oldfold);
+onset_times_data= onset_detect(data, fs, windowSize, hopSize, minTime) ;
+onset_times_percuss = onset_detect(p, fs, windowSize, hopSize, minTime) ;
+onset_times_percuss=onset_times_percuss/fs;
